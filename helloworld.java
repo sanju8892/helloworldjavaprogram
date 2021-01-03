d1103ba650d16e3a92c99b3a0042f3eaa71993cd
DevOpsG <76554480+DevOpsG4u@users.noreply.github.com>
2021-01-03 14:41:58 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,6 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello batch21 team");
+      System.out.println("Hello batch21 students and training practice with nishi and jitendra an abcdefghi and some jobs for new build testing 2");
+    }  
+}  
